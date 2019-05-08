8f5c3ef36b8f56a64ee458415d81f28d4bece7c6
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-05-08 15:29:32 -0400

    added styled form

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -2,30 +2,97 @@ import React from "react";
 import axios from "axios";
 
 export default class Friends extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        friends: []
-      };
-    }
-    componentDidMount() {
-      axios
-        .get("http://localhost:5000/friends")
-        .then(res => {
-          this.setState({
-            friends: res.data
-          });
-        })
-        .catch(err => console.log(err));
-    }
-    render() {
-      return (
-        <div>
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => {
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  submit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+  render() {
+    return (
+      <>
+        <div className="people-container">
           {this.state.friends &&
             this.state.friends.map(e => {
-              return <div>{JSON.stringify(e)}</div>;
+              return (
+                <div key={e.id} className="people">
+                  <div>
+                    {e.name}, {e.age}, {e.email}
+                  </div>
+                </div>
+              );
             })}
         </div>
-      );
-    }
+        <>
+          {!this.state.update && (
+            <form action="submit">
+              <input
+                type="text"
+                onChange={this.name}
+                value={this.state.name}
+                placeholder="Name"
+              />
+              <input
+                type="number"
+                onChange={this.age}
+                value={this.state.age}
+                placeholder="Age"
+              />
+              <input
+                type="email"
+                onChange={this.email}
+                value={this.state.email}
+                placeholder="Email"
+              />
+              <button onClick={this.submit} onSubmit={this.submit}>
+                Submit
+              </button>
+            </form>
+          )}
+          {this.state.update && (
+            <div>
+              <h2>Update</h2>
+              <form action="submit">
+                <input
+                  type="text"
+                  onChange={this.name}
+                  value={this.state.name}
+                  placeholder="Name"
+                />
+                <input
+                  type="number"
+                  onChange={this.age}
+                  value={this.state.age}
+                  placeholder="Age"
+                />
+                <input
+                  type="email"
+                  onChange={this.email}
+                  value={this.state.email}
+                  placeholder="Email"
+                />
+                <button onClick={this.update} onSubmit={this.update}>
+                  Update
+                </button>
+              </form>
+            </div>
+          )}
+        </>
+      </>
+    );
+  }
 }
